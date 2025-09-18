80f2312d770f66b59a2021e1f15beec12cb8109e
junseo511 <prime54@g.skku.edu>
2025-09-19 00:38:25 +0900

    🎉🎉 init 🎉🎉

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
