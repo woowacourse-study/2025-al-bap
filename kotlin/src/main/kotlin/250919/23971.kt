12e017dd4721cd5ea8e086b3d48d5d1fc87d94c0
junseo511 <prime54@g.skku.edu>
2025-09-19 00:43:04 +0900

    23971 풀이 완료

---
+++ kotlin/src/main/kotlin/250919/23971.kt
@@ -0,0 +1,10 @@
+package `250919`
+
+fun main() {
+    val a = readln().split(" ").map { it.toInt() }
+    println(solve(a[0], a[1], a[2], a[3]))
+}
+
+fun solve(h: Int, w: Int, n: Int, m: Int): Int {
+    return ((w - 1) / (m + 1) + 1) * ((h - 1) / (n + 1) + 1)
+}
