64a7e72766dbc8fc752934060b0e3c5431fdf055
junseo511 <prime54@g.skku.edu>
2025-09-19 18:07:34 +0900

    2816 풀이 완료

---
+++ kotlin/src/main/kotlin/250919/2816.kt
@@ -0,0 +1,36 @@
+package `250919`
+
+fun main() {
+    val count: Int = readln().toIntOrNull() ?: return
+    val channels: MutableList<String> = mutableListOf()
+    for (i in 0 until count) {
+        val input = readln()
+        channels.add(input)
+    }
+    println(solve(channels))
+}
+
+fun solve(list: MutableList<String>):String {
+    var answer: String = ""
+    val pos1: Int = list.indexOf("KBS1")
+
+    for (i in 0 until pos1) {
+        answer += "1"
+    }
+    for (i in 0 until pos1) {
+        answer += "4"
+    }
+    list.removeAt(pos1)
+    list.add(0, "KBS1")
+
+    val pos2: Int = list.indexOf("KBS2")
+
+    for (i in 0 until pos2) {
+        answer += "1"
+    }
+    for (i in 0 until pos2 - 1) {
+        answer += "4"
+    }
+
+    return answer
+}
